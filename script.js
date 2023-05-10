e2dcd1314a03b987dc2547eca51e2044ac44a6f0
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-05-10 12:33:17 -0400

    add event listener to update cookie rating in db.json when selected from dropdown

--- script.js
+++ script.js
@@ -1,43 +1,62 @@
 //create DOM element to contain each cookie
 const cardBuilder = (cookie) => {
-        let cardContainer = document.getElementById("cookie-container")
-        let cookieCard = document.createElement("div")
-        cookieCard.classList.add("cookie-card")
+    let cardContainer = document.getElementById("cookie-container")
+    let cookieCard = document.createElement("div")
+    cookieCard.classList.add("cookie-card")
 
-        let img = document.createElement("img")
-        img.src = cookie.image
+    let img = document.createElement("img")
+    img.src = cookie.image
 
-        let flavor = document.createElement("h3")
-        flavor.innerText = cookie.flavor
+    let flavor = document.createElement("h3")
+    flavor.innerText = cookie.flavor
 
-        let p = document.createElement("p")
-        p.textContent = cookie.description
+    let p = document.createElement("p")
+    p.textContent = cookie.description
 
-        const selectArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        const ratingLabel = document.createElement("span")
-        ratingLabel.innerText = "Rating:"
-        const userRating = document.createElement("select");
-        for (let i = 0; i < 10; i++) {
-            let option = document.createElement("option");
-            option.value = selectArray[i];
-            option.text = selectArray[i];
-            userRating.appendChild(option);
-        }
-        userRating.value = cookie.rating;
+    const selectArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const ratingLabel = document.createElement("span")
+    ratingLabel.innerText = "Rating:"
+    const userRating = document.createElement("select");
+    for (let i = 0; i < 10; i++) {
+        let option = document.createElement("option");
+        option.value = selectArray[i];
+        option.text = selectArray[i];
+        userRating.appendChild(option);
+    }
+    userRating.value = cookie.rating;
+    // Event listener for updating rating in db.json
+    userRating.addEventListener("change", (e) => {
+    if (e.target === userRating) {
+        const newRating = e.target.value;
+        const cookieId = cookie.id;
+        fetch(`http://localhost:3000/Cookies/${cookieId}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                rating: newRating
+            })
+        })
+        .then(r => r.json())
+        .then(updatedCookie => console.log(updatedCookie))
+        .catch(error => console.log(error))
+    }
+});
 
 
-        cookieCard.appendChild(flavor)
-        cookieCard.appendChild(img)
-        cookieCard.appendChild(p)
-        cookieCard.appendChild(ratingLabel)
-        cookieCard.appendChild(userRating)
-        cardContainer.appendChild(cookieCard)
+cookieCard.appendChild(flavor)
+cookieCard.appendChild(img)
+cookieCard.appendChild(p)
+cookieCard.appendChild(ratingLabel)
+cookieCard.appendChild(userRating)
+cardContainer.appendChild(cookieCard)
 }
 // fetch cookies, submit new cookies
 document.addEventListener("DOMContentLoaded", () => {
     fetch("http://localhost:3000/cookies")
-    .then(r => r.json())
-    .then(cookies => cookies.forEach(cookie => cardBuilder(cookie)))
+        .then(r => r.json())
+        .then(cookies => cookies.forEach(cookie => cardBuilder(cookie)))
     const form = document.querySelector("form.cookie-form");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -65,6 +84,6 @@ function submitForm(newCookie) {
             "image": newCookie.image
         })
     })
-    .then(r => r.json())
-    .then(responseCookie => cardBuilder(responseCookie))
+        .then(r => r.json())
+        .then(responseCookie => cardBuilder(responseCookie))
 }
